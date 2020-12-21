d11a62bd69ff0667317591f1b80ee2ef0376173b
Daniel Perrefort <djperrefort@pitt.edu>
2020-12-21 13:28:26 -0500

    Clarification edits to docsting in node.py

--- simple_pipe/nodes.py
+++ simple_pipe/nodes.py
@@ -3,25 +3,26 @@ from __future__ import annotations
 import abc
 import inspect
 import multiprocessing as mp
+from abc import ABC
 from itertools import chain
 from typing import List, Union, cast
 
 from . import connectors
 
 
-class NodeBase:
+class Node(abc.ABC):
     """Base class for constructing pipeline nodes"""
 
     def __init__(self) -> None:
-        """Represents a single pipeline node"""
+        """Represents a single pipeline _node"""
 
         self.validate()
 
         for source in self.input_connections():
-            source.node = self
+            source._node = self
 
         for target in self.output_connections():
-            target.node = self
+            target._node = self
 
         self.num_processes = 1
         self.processes: List[mp.Process] = []
@@ -42,7 +43,7 @@ class NodeBase:
         return all(c.connected() for c in chain(self.input_connections(), self.output_connections()))
 
     def input_connections(self) -> List[connectors.Input]:
-        """Returns a list of input connections assigned to the current node
+        """Returns a list of input connections assigned to the current _node
 
         Returns:
             A list of ``Input`` connection objects
@@ -52,7 +53,7 @@ class NodeBase:
         return [getattr(self, a[0]) for a in inspect.getmembers(self, predicate)]
 
     def output_connections(self) -> List[connectors.Output]:
-        """Returns a list of output connections assigned to the current node
+        """Returns a list of output connections assigned to the current _node
 
         Returns:
             A list of ``Output`` connection objects
@@ -62,15 +63,15 @@ class NodeBase:
         return [getattr(self, a[0]) for a in inspect.getmembers(self, predicate)]
 
     def input_nodes(self) -> Union[Source, Inline]:
-        """Returns a list of upstream pipeline nodes connected to the current node"""
+        """Returns a list of upstream pipeline nodes connected to the current _node"""
 
-        nodes = [c.node for c in self.input_connections()]
+        nodes = [c._node for c in self.input_connections()]
         return cast(Union[Source, Inline], nodes)
 
     def output_nodes(self) -> Union[Inline, Target]:
-        """Returns a list of downstream pipeline nodes connected to the current node"""
+        """Returns a list of downstream pipeline nodes connected to the current _node"""
 
-        nodes = [c.node for c in self.output_connections()]
+        nodes = [c._node for c in self.output_connections()]
         return cast(Union[Inline, Target], nodes)
 
     def setup(self) -> None:
@@ -89,13 +90,13 @@ class NodeBase:
 
         pass
 
-    def __call__(self):
+    def run(self):
         self.setup()
         self.action()
         self.teardown()
 
 
-class Source(NodeBase, metaclass=abc.ABCMeta):
+class Source(Node, ABC):
     """A pipeline process that only has output streams"""
 
     def validate(self) -> None:
@@ -109,7 +110,7 @@ class Source(NodeBase, metaclass=abc.ABCMeta):
             raise ValueError('Source objects cannot have upstream components')
 
 
-class Target(NodeBase, metaclass=abc.ABCMeta):
+class Target(Node, ABC):
     """A pipeline process that only has input streams"""
 
     def validate(self) -> None:
@@ -123,7 +124,7 @@ class Target(NodeBase, metaclass=abc.ABCMeta):
             raise ValueError('Source objects cannot have upstream components')
 
 
-class Inline(Source, Target, metaclass=abc.ABCMeta):
+class Inline(Source, Target, ABC):
     """A pipeline process that can have any number of input or output streams"""
 
     def validate(self) -> None:
