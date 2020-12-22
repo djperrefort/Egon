f29f9ade34467c0b5e02fdb15f208669323348bc
Daniel Perrefort <djperrefort@pitt.edu>
2020-12-22 08:08:26 -0500

    Adds install logic

--- egon/__init__.py
+++ egon/__init__.py
@@ -1,1 +1,7 @@
+"""Egon is a lightweight, minimalist framework for the rapid development
+of data ana;lysis pipelines.
+"""
 
+__version__ = '0.0.1'
+__author__ = 'Daniel Perrefort'
+__license__ = 'GPL 3.0'
