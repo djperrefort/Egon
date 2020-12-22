491e2222f46ec7be6dbf87dd4fde78d3c84e1444
Daniel Perrefort <djperrefort@pitt.edu>
2020-12-22 09:55:15 -0500

    Extends datastore tests

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -1,27 +1,37 @@
 from unittest import TestCase
-
+import time
 from egon.connectors import DataStore
 
 
 class QueueProperties(TestCase):
+    """Test  test the exposure of queue properties by the overlying ``DataStore`` class"""
 
     def setUp(self) -> None:
-        self.data_store = DataStore()
+        """Create a ``DataStore`` instance"""
+
+        self.data_store = DataStore(maxsize=1)
 
     def test_size_matches_queue_size(self) -> None:
-        self.assertEqual(self.data_store.size(), self.data_store._queue.qsize())
-        self.data_store._queue.put(1)
-        self.assertEqual(self.data_store.size(), self.data_store._queue.qsize())
-
-    def test_full_method_matches_queue_state(self) -> None:
-
-        data_store = DataStore(maxsize=1)
-        self.assertFalse(data_store.full())
-        data_store._queue.put(1)
-        self.assertTrue(data_store.full())
-
-    def test_size_method_matches_queue_state(self) -> None:
+        """Test the ``size`` method returns the size of the queue`"""
 
         self.assertEqual(self.data_store.size(), 0)
         self.data_store._queue.put(1)
         self.assertEqual(self.data_store.size(), 1)
+
+    def test_full_state(self) -> None:
+        """Test the ``full`` method returns the state of the queue"""
+
+        self.assertFalse(self.data_store.full())
+        self.data_store._queue.put(1)
+        self.assertTrue(self.data_store.full())
+
+    def test_empty_state(self) -> None:
+        """Test the ``empty`` method returns the state of the queue"""
+
+        self.assertTrue(self.data_store.empty())
+        self.data_store._queue.put(1)
+
+        # The value of Queue.empty() updates asynchronously
+        time.sleep(1)
+
+        self.assertFalse(self.data_store.empty())
