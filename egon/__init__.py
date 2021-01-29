845ac5992d2745a852741fc98c9ace712b58dbeb
Daniel Perrefort <djperrefort@pitt.edu>
2021-01-29 11:35:59 -0500

    Patch bump

--- egon/__init__.py
+++ egon/__init__.py
@@ -3,6 +3,6 @@ analysis pipelines. See the online documentation at
 https://mwvgroup.github.io/Egon/
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Daniel Perrefort'
 __license__ = 'GPL 3.0'
